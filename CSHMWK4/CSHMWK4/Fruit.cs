00b242bd18bc95029d08e11717303cdbd948e618
GLor1 <50379998+GLor1@users.noreply.github.com>
2019-05-31 15:29:27 -0500

    Add files via upload

---
+++ CSHMWK4/CSHMWK4/Fruit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSHMWK4
+{
+   public class Fruit
+    {
+        public string _seeds { set; get; }
+        public string _flavor { set; get; }
+        public int _childrenProduced { set; get; }
+
+        public Fruit(string seeds, string flavor, int childrenProduced)
+        {
+            _seeds = seeds;
+            _flavor = flavor;
+            _childrenProduced = childrenProduced;
+            Console.WriteLine("Seeds: {0}, flavor: {0}, childrenProduced {0}", _seeds, _flavor, _childrenProduced);
+        }
+        public void childP(int babiesMadefromconsumingfruit)
+        {
+            for(int i = 0; i < babiesMadefromconsumingfruit; i++)
+            {
+                Console.Write("Count this line to see how many children you'll have. {0}", _childrenProduced);
+            }
+        }
+        public void flavor(string flavor)
+        {
+            Console.WriteLine("Sweet!");
+        }                                        
+    }
+
+
+    public class Mango: Fruit
+    {
+    public string _taste { set; get; }
+    public Mango(string seeds, string flavor, int childrenProduced, string taste) : base (seeds, flavor, childrenProduced)
+    {
+        _taste = taste;
+        Console.WriteLine("This mango tastes great {0}.", _taste);
+    }
+    public void action(string action)
+    {
+        Console.WriteLine("The mango goes man!", action);
+    }
+    }
+        
+     
+
+    
+}
---
+++ CSHMWK4/CSHMWK4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSHMWK4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit f1 = new Fruit ("ten", "sweet", 10);
+            f1.childP(2);
+
+            Mango app = new Mango("one", "sour", 12, "yummy");
+        }
+    }
+}
